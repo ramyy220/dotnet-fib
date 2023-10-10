0dd97a5af000a11cc59a22ff9061a619e7054568
Ramyy220 <ramyamr70@gmail.com>
2023-10-10 15:00:22 +0200

    Update ef

--- src/Leonardo/Fibonacci.cs
+++ src/Leonardo/Fibonacci.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace Leonardo;
 
@@ -13,28 +14,54 @@ public class Fibonacci
 
     public static async Task<IList<int>> RunAsync(string[] args)
     {
+        await using var context = new FibonacciDataContext();
+        
         IList<int> results = new List<int>();
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
         var tasks = new List<Task<int>>();
-        foreach(var arg in args)
+        foreach (var arg in args)
         {
-            var task = Task.Run(() =>
+            var tfibonacci = await context.TFibonaccis
+                .Where(t => t.FibInput == int.Parse(arg))
+                .FirstOrDefaultAsync();
+
+            if (tfibonacci == null)
             {
-                var result = Fibonacci.Run(int.Parse(arg));
-                Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms {arg}");
-                return result;
-            });
-            tasks.Add(task);
+
+
+
+                var task = Task.Run(() =>
+                {
+                    var result = Fibonacci.Run(int.Parse(arg));
+                    Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms {arg}");
+                    return result;
+                });
+                tasks.Add(task);
+            }
+            else
+            {
+                tasks.Add(Task.FromResult((int)tfibonacci.FibOutput));
+            }
         }
+
         foreach (var task in tasks)
         {
+            
             var result = await task;
+            context.TFibonaccis.Add(new TFibonacci()
+            {
+                FibOutput = result,
+                FibInput = int.Parse(args[tasks.IndexOf(task)]),
+                
+            });
             Console.WriteLine($"Result: {task.Result}");
             results.Add(task.Result);
         }
         stopwatch.Stop();
         Console.WriteLine("Total elapsed time: {0} ms", stopwatch.ElapsedMilliseconds);
+        
+        await context.SaveChangesAsync();
 
         return results;
     }
