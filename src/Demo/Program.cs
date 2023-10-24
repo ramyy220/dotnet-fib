51e6eaa3852362bd59e374aeb03209af912bb8cd
Ramyy220 <ramyamr70@gmail.com>
2023-10-24 15:47:30 +0200

    last

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Leonardo;
 
-var results = Fibonacci.RunAsync(args);
+using var context = new FibonacciDataContext();
+
+var results = new Fibonacci(context).RunAsync(args);
 Console.WriteLine($"Finished");
 results.Wait();
