075456278a09f8d943abb4bceaa160d3d947b87c
Ramyy220 <ramyamr70@gmail.com>
2023-10-24 16:00:57 +0200

    STP

--- tests/Leonardo.tests/UnitTest1.cs
+++ tests/Leonardo.tests/UnitTest1.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Leonardo.tests;
 
 public class UnitTest1
 {
     [Fact]
-    public async Task Test1()
+    public  async Task Test1()
     {
-        var results = await Fibonacci.RunAsync(new string[] { "44", "22" });
+        var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
+        var dataBaseName = Guid.NewGuid().ToString();
+        builder.UseInMemoryDatabase(dataBaseName);
+
+        var options = builder.Options;
+        var fibonacciDataContext = new FibonacciDataContext(options);
+        await fibonacciDataContext.Database.EnsureCreatedAsync();
+        
+        
+        var results = await new Fibonacci(fibonacciDataContext).RunAsync(new string[] { "44", "22" });
         Console.WriteLine($"Finished");
         Assert.Equal(701408733, results[0]);
         Assert.Equal(17711, results[1]);
